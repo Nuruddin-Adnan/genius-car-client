7aac88457ea42282cf643f27e96482e6862f3a05
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-11-03 01:40:26 +0600

    project initial setpu

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaBeer } from 'react-icons/fa';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            Home page <FaBeer />
+        </div>
+    );
+};
+
+export default Home;

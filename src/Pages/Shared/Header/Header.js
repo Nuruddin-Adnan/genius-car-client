da2224a6dd3db0cc34a8fcab40403da16e8a664b
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-11-03 09:05:58 +0600

    firebase install

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            Header
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Sign UP</Link>
         </div>
     );
 };
